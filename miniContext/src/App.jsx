4dffc819a9a10f48aa2f83d960dc260835e1db6e
neerajpandit <pandeyneeraj1507@gmail.com>
2024-01-07 22:05:55 +0530

    themechanger

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -6,8 +6,7 @@ import Login from './components/Login'
 import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <UserContextProvider>
       
